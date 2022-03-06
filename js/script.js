94d304df5ebefbce193dd6ff16f03c0871cdc967
JKLLR <jeffhuria@gmail.com>
2022-03-07 02:36:56 +0300

    add date validation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-var birthday = document.getElementById("birthday");
-var output = document.getElementById("outPut")
+var birthday = document.getElementById("birthday").value;
+var output = document.getElementById("outPut");
 var sel = document.getElementById('gender');
 var submitButton = document.getElementById("submit");
 
-var submitDates = ()=> {
-// check if date is selected
-     if(!birthday.value){
-         alert("You need to enter a date!")
-  }
-var userDob = new Date(birthday.value);
-
+var submitDates = () => {
+  //check if date is selected
+      if (birthday == '') {
+          alert("you need to enter a date!")
+      }
+    }
