484589e69805235fb183c1d10ee5a6391c0c51ea
JKLLR <jeffhuria@gmail.com>
2022-03-06 23:39:19 +0300

    add var userDob

--- js/script.js
+++ js/script.js
@@ -5,5 +5,6 @@ var submitButton = document.getElementById("submit");
 
 var submitDates = ()=> {
 // check if date is selected
-     
+     if(!birthday.value){
+         alert("You need to enter a date!")
 }
