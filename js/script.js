8b00a841fd326428e8f3996d4f3f8d91552a6ff6
JKLLR <jeffhuria@gmail.com>
2022-03-06 23:18:32 +0300

    add date control

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
-var day = document.getElementById("day");
+var day = document.getElementsByName("day")[0]
 var month = document.getElementById("month");
 var year = document.getElementById("year");
+var output = document.getElementById("outPut")
+var sel = document.getElementById('gender');
 var submitButton = document.getElementById("submit");
-var submitDeets = () => {
-  //check if month is valid
-  if (month.value < 1 || month.value > 12) {
-    console.log(month.value);
-    alert("invalid date:Check month");
-  }
-  //check if day is valid
-  if (day.value < 1 || day.value > 31) {
-    console.log(day.value);
-    alert("invalid date:Check day");
-  }
-  //check if year is valid
-  if (year.value < 1600 || year.value > 2100) {
-    console.log(year.value);
-    alert("invalid date:Check year");
-  }
-};
