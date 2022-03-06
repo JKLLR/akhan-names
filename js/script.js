e086217924413c2d1924e4d7a2dc9da15f996b37
JKLLR <jeffhuria@gmail.com>
2022-03-06 17:17:41 +0300

    validate year input

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-var day = document.getElementById("day").value;
-var month = document.getElementById("month").value;
-var year = document.getElementById("year").value;
+var day = document.getElementById("day");
+var month = document.getElementById("month");
+var year = document.getElementById("year");
 var submitButton = document.getElementById("submit");
 var submitDeets = () => {
   //check if month is valid
-  if (month < 1 || month > 12) {
-    alert("invalid date");
-  }
+  if (month.value < 1 || month.value > 12) {
+      console.log(month.value)
+    alert("invalid date:Check month");
+  };
+  //check if day is valid
+  if (day.value <1 || day.value > 31) {
+       console.log(day.value)
+    alert("invalid date:Check day");
+  };
 };
