7b8375aa5074a12eceebccc97017d3fde5f58b4a
JKLLR <jeffhuria@gmail.com>
2022-03-07 07:19:42 +0300

    debug js file

--- js/script.js
+++ js/script.js
@@ -1,52 +1,51 @@
-var birthday = document.getElementById("birthday").value;
+var birthday = document.getElementById("birthday");
 var output = document.getElementById("outPut");
-var sel = document.getElementById('gender');
+var sel = document.getElementById("gender");
 var submitButton = document.getElementById("submit");
 
 var submitDates = () => {
   //check if date is selected
-      if (birthday == "") {
-          alert("you need to enter a date!")
-      };
+  if (!birthday.value) {
+    alert("you need to enter a date!");
+  }
   var userDob = new Date(birthday.value);
-  console.log(userDob);
 
-  CC = userDob.getFullYear();
-  CC = CC.toString().substr(0, 1);
-  console.log(CC);
+  var CC = parseInt(
+    userDob.getFullYear().toString().split("")[0] +
+      userDob.getFullYear().toString().split("")[1]
+  );
 
-  YY = userDob.getFullYear();
-  YY = YY.toString().substr(2, 3);
-  console.log(YY);
+  var YY = parseInt(
+    userDob.getFullYear().toString().split("")[2] +
+      userDob.getFullYear().toString().split("")[3]
+  );
 
-  MM = userDob.getMonth();
-  console.log(MM);
+  var MM = userDob.getMonth();
 
-  DD = userDob.getDate();
-  console.log(DD);
-   
-  var result = (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
+  var DD = userDob.getDate();
 
+  var result =
+    Math.trunc(CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) %
+    7;
 
-  var males = ["Kwasi","Kwadwo","Kwabena","Kwaku"," Yaw","Kofi","Kwame"]
+  var males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame"];
+  var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-  var females = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-
-  if(getSelectedOption(sel).value == "male"){
-    output.innerHTML = `Your akan name is:${males[result]}`
-  }else {
-    output.innerHTML = `Your akan name is:${females[result]}`
+  if (getSelectedOption(sel).value == "male") {
+    output.innerHTML = `Your akan name is:${males[result]}`;
+  } else {
+    output.innerHTML = `Your akan name is:${females[result]}`;
   }
 };
 
 //gets the gender option
 function getSelectedOption(sel) {
-    var opt;
-    for ( var i = 0, len = sel.options.length; i < len; i++ ) {
-        opt = sel.options[i];
-        if ( opt.selected === true ) {
-            break;
-        }
+  var opt;
+  for (var i = 0, len = sel.options.length; i < len; i++) {
+    opt = sel.options[i];
+    if (opt.selected === true) {
+      break;
     }
-    return opt;
+  }
+  return opt;
 }
