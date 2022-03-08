8f7e5061b7835769704e5edc24bc16dd002b3f28
JKLLR <jeffhuria@gmail.com>
2022-03-08 08:35:08 +0300

    a revised js

--- js/script.js
+++ js/script.js
@@ -1,51 +1,61 @@
-var birthday = document.getElementById("birthday");
-var output = document.getElementById("outPut");
-var sel = document.getElementById("gender");
-var submitButton = document.getElementById("submit");
+// array to represent Akan names
+let males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame"];
 
-var submitDates = () => {
-  //check if date is selected
-  if (!birthday.value) {
-    alert("you need to enter a date!");
+let females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let inputAction = document.getElementById("submit").addEventListener("click", submitDates);
+
+function submitDates() {
+  let birthday = document.getElementById("birthday").value;
+  let d = new Date(birthday);
+  let day = d.getDay()
+  let gender = document.getElementById("gender").value;
+
+  if (isNaN(d) || gender == "") {
+    alert("you need to enter a date and gender!");
+  } 
+  else if (gender == "male" && day == 0) {
+    outPut.textContent = `Your akan name is:${males[0]}`
   }
-  var userDob = new Date(birthday.value);
-
-  var CC = parseInt(
-    userDob.getFullYear().toString().split("")[0] +
-      userDob.getFullYear().toString().split("")[1]
-  );
-
-  var YY = parseInt(
-    userDob.getFullYear().toString().split("")[2] +
-      userDob.getFullYear().toString().split("")[3]
-  );
-
-  var MM = userDob.getMonth();
-
-  var DD = userDob.getDate();
-
-  var result =
-    Math.trunc(CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) %
-    7;
-
-  var males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame"];
-  var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-  if (getSelectedOption(sel).value == "male") {
-    output.innerHTML = `Your akan name is:${males[result]}`;
-  } else {
-    output.innerHTML = `Your akan name is:${females[result]}`;
+   else if (gender == "female" && day == 0) {
+    outPut.textContent = `Your akan name is:${females[0]}`
   }
-};
-
-//gets the gender option
-function getSelectedOption(sel) {
-  var opt;
-  for (var i = 0, len = sel.options.length; i < len; i++) {
-    opt = sel.options[i];
-    if (opt.selected === true) {
-      break;
-    }
+  else if (gender == "male" && day == 1) {
+    outPut.textContent = `Your akan name is:${males[1]}`;
   }
-  return opt;
+  else if (gender == "female" && day == 1) {
+    outPut.textContent = `Your akan name is:${females[1]}`
+  }
+  else if (gender == "male" && day == 2) {
+    outPut.textContent = `Your akan name is:${males[2]}`
+  }
+  else if (gender == "female" && day == 2) {
+    outPut.textContent = `Your akan name is:${females[2]}`
+  }
+  else if (gender == "male" && day == 3) {
+    output.textContent = `Your akan name is:${males[3]}`
+  }
+  else if (gender == "female" && day == 3) {
+    output.textContent = `Your akan name is:${males[3]}`
+  }
+  else if (gender == "male" && day == 4) {
+    outPut.textContent = `Your akan name is:${males[4]}`
+  }
+  else if (gender == "female" && day == 4) {
+    outPut.textContent = `Your akan name is:${females[4]}`
+  }
+  else if (gender == "male" && day == 5) {
+    outPut.textContent = `Your akan name is:${males[5]}`
+  }
+  else if (gender == "female" && day == 5) {
+    outPut.textContent = `Your akan name is:${females[5]}`
+  }
+  else if (gender == "male" && day == 6) {
+    outPut.textContent = `Your akan name is:${males[6]}`
+  }
+  else if (gender == "female" && day == 6) {
+    outPut.textContent = `Your akan name is:${females[6]}`
+  }
 }
